384490d3ee54e3c81b3169816af378ab50a8bff5
Özgür Hasan Aytar <zrhaytar@gmail.com>
2017-11-12 01:04:41 +0300

    main page

---
+++ app/build/generated/source/buildConfig/androidTest/debug/com/group1/ybe/artwork/test/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.group1.ybe.artwork.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.group1.ybe.artwork.test";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
